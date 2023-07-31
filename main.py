5eb42b153508efd3d1e7521b36580bed5a5f14fe
Luan Thierry S. Pacifico <luanpacifico@gmail.com>
2023-07-31 13:41:07 -0300

    Finished Search Entry filtered by SQL query

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from MyWidgets import Table
 from CTkToolTip import CTkToolTip
 import sqlite3
 import pandas as pd
+import random
 
 class App(ctk.CTk):
     def __init__(self):
@@ -18,16 +19,27 @@ class App(ctk.CTk):
         
 
         # widgets
-        con = sqlite3.connect('MyDb.db') 
-        search = SearchInDataBase(master=self, by='name', con=con, table_name='clientes', display_table_cols=['nome', 'idade'])
+        con = sqlite3.connect('MyDb.db')
+        # DEBUG populate database
+        # cursor = con.cursor()
+        # cursor.execute('CREATE TABLE clientes (nome text, sobrenome text, cpf integer, idade integer)')
+        # for _ in range(100):
+        #     cpf = random.randint(00000000000, 99999999999)
+        #     nome = random.choice(['Paulo', 'Luan', 'Carlos', 'Adalberto', 'Fabio', 'Maria', 'João', 'Sabrina'])
+        #     sobrenome = random.choice(['Garcia', 'Pereira', 'Toretto', 'Lima', 'Andrade', 'Santos', 'Silva', 'Redentor'])
+        #     idade = random.randint(10, 90)
+        #     cursor.execute(f"INSERT INTO clientes VALUES ('{nome}', '{sobrenome}', {cpf}, {idade})")
+        # con.commit()
+        search = SearchInDataBase(master=self, by='cpf', con=con, table_name='clientes', display_table_cols=['nome', 'sobrenome', 'idade', 'cpf'])
         search.grid(row=0, column=1, sticky='nswe')
 
         # run
         self.mainloop()
+    
 
 
 class SearchInDataBase(ctk.CTkFrame):
-    def __init__(self, master, con: sqlite3.Connection, by: str, table_name: str, display_table_cols: list[str]):
+    def __init__(self, master, con: sqlite3.Connection, by: str, table_name: str, display_table_cols: list):
         super().__init__(master=master)
         self.by = by
         self.con = con
@@ -51,7 +63,7 @@ class SearchInDataBase(ctk.CTkFrame):
 
         frame1.grid(row=0, column=0, sticky='nswe', padx=5, pady=5)
 
-        entry = ctk.CTkEntry(master=frame1, placeholder_text='Enter a Name', textvariable=self.nome)
+        entry = ctk.CTkEntry(master=frame1, placeholder_text='Enter a Name', textvariable=self.str_entry)
         entry.grid(row=0, column=0, sticky='nswe', padx=5, pady=5)
 
         btn = ctk.CTkButton(master=frame1, text="Search", command=lambda: self.update_query(table=table, con=con))
@@ -61,7 +73,8 @@ class SearchInDataBase(ctk.CTkFrame):
         frame2 = ctk.CTkScrollableFrame(master=self)
         frame2.grid(row=1, column=0, sticky='nswe')
         
-        query = pd.read_sql(f"SELECT * FROM {self.table_name}", con=self.con)
+        query_str = self.select_all_sql()
+        query = pd.read_sql(query_str, con=self.con)
         
         table = Table(master=frame2, values=query) 
         table.pack(fill='both', expand=True)
@@ -72,7 +85,7 @@ class SearchInDataBase(ctk.CTkFrame):
 
     
     def update_query(self, table: Table, con):
-        query = pd.read_sql(f"SELECT nome, idade FROM clientes WHERE nome LIKE '%{self.nome.get()}%'", con=con)
+        query = pd.read_sql(f"{self.select_all_sql()} WHERE {self.by} LIKE '%{self.str_entry.get()}%'", con=con)
         new_data = query.to_numpy().tolist()
         new_data.insert(0, query.columns.values.tolist())
         table.update_values(new_data)
@@ -81,7 +94,9 @@ class SearchInDataBase(ctk.CTkFrame):
         tables_str = ''
         for table in self.display_table_cols:
             tables_str += table + ','
-        tables_str = tables_str[:-2]
+        tables_str = tables_str[:-1]
+        return f"SELECT {tables_str} FROM {self.table_name}"
+
 
 
 
